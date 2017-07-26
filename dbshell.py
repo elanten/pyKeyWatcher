6563477b88ec981630d8ad44de7e8c5da306d876
Anton M <elantenvil@gmail.com>
2017-07-26 10:38:44 +0300

    Тесты для страницы редактирования. Куча мелких правок.

--- dbshell.py
+++ dbshell.py
@@ -1,56 +1,51 @@
+# Файл для работы с тестовой базой
+
 from digital_key.models import *
 from employee.models import *
 from cert_center.models import *
 from key_manual.models import *
 from django.utils import dateparse, timezone
+import re, random
+import datetime as dt
 
-c1 = ContactType(name='email')
-c2 = ContactType(name='post')
 
-c1.save()
-c2.save()
+def randomize_names():
+    count = 1
+    tpls = ['eTocken_{:02d}', 'ruTocken_{:02d}', 'Flash_{:02d}']
+    tDict = {key: 0 for key in tpls}
+    for key in DigitalKey.objects.all():
+        key.serial = f'1234-xxx-{count:02d}'
+        count += 1
+        tpl: str = random.choice(tpls)
+        pos = tDict[tpl] = tDict[tpl] + 1
+        key.name = tpl.format(pos)
+        key.save()
+        count += 1
 
-emp1 = Employee(name='ФИО1')
-emp2 = Employee(name='ФИО2')
 
-emp1.save()
-emp2.save()
+def randomize_dates():
+    for key in DigitalKey.objects.all():
+        now = dt.datetime.now()
+        _start = now - dt.timedelta(days=random.randint(5, 10))
+        _end = now + dt.timedelta(days=random.randint(15, 90))
+        key.date_begin = _start.date()
+        key.date_expire = _end.date()
+        key.renewal_time = random.randint(5, 15)
+        key.save()
 
-emp1.contactinfo_set.create(value='test@mail.io', type=c1)
-emp1.contactinfo_set.create(value='123 45 67', type=c2)
 
-grp1 = EmployeeGroup.objects.create(name='Группа 1', description="1123")
-grp1.members.add(emp1, emp2)
-
-k1 = KeyType.objects.create(name="eToken")
-k2 = KeyType.objects.create(name="ruToken")
-
-ka1 = KeyAssignment.objects.create(name="Госсакупки")
-ka2 = KeyAssignment.objects.create(name="Росчтототам")
-
-kl1 = KeyLocation.objects.create(name="Склад 1")
-kl2 = KeyLocation.objects.create(name="Склад 2")
-
-cent1 = CertificationCenter.objects.create(name='Удостоверяющий центр 1',
-                                           link='http://moscow.roskazna.ru/gis/uc',
-                                           description='123')
-
-cent2 = CertificationCenter.objects.create(name='Удостоверяющий центр 2', description='1234')
-
-cr1 = CertRequirement.objects.create(name='Требования 1', center=cent1, description='123',
-                                     actual_date=timezone.now())
-
-key1 = DigitalKey(name="Ключ 1", serial='id-098765', type=k1, assignment=ka1, location=kl1, renewal_time=14,
-                  cert_holder=emp1, key_receiver=emp1, employee_group=grp1, date_expire=timezone.now(),
-                  cert_center=cent1)
-key1.save()
-key2 = DigitalKey(name="Ключ 2", serial='id-12345', type=k2, assignment=ka2, location=kl2, copy_of=key1)
-key2.save()
-
-mt1 = ManualType.objects.create(name='Установка')
-mt2 = ManualType.objects.create(name='Использование')
-mt3 = ManualType.objects.create(name='Ключи')
-
-man1 = Manual.objects.create(name="Инструкция 1", type=mt1, content="Краткое описание\nинструкции",
-                             link='http://www.roskazna.ru/gis/')
-man2 = Manual.objects.create(name="Инструкция 2")
+def randomize_links():
+    systems = [sys for sys in WorkSystem.objects.all()]
+    assignments = [ass for ass in KeyAssignment.objects.all()]
+    locs = [loc for loc in KeyLocation.objects.all()]
+    types = [typ for typ in KeyType.objects.all()]
+    centers = [cen for cen in CertificationCenter.objects.all()]
+    employees = [emp for emp in Employee.objects.all()]
+    for key in DigitalKey.objects.all():
+        key.work_systems.add(random.choice(systems))
+        key.assignment = random.choice(assignments)
+        key.location = random.choice(locs)
+        key.type = random.choice(types)
+        key.cert_center = random.choice(centers)
+        key.cert_holder = random.choice(employees)
+        key.save()
--- digital_key/tests/test_base.py
+++ digital_key/tests/test_base.py
@@ -1,18 +1,25 @@
+from datetime import datetime
+
 from django.contrib.auth.models import User
 from django.test import TestCase
 # Create your tests here.
 from django.urls import reverse
 
+from digital_key.forms import DigitalKeyForm
+from digital_key.models import DigitalKey
 from digital_key.tests.utils import create_key, create_location, create_work_system, create_assignment
 
 URL_DIGITAL_KEY_ALL = 'digital_key:all'
 URL_DIGITAL_KEY_DETAIL = 'digital_key:show_by_id'
+URL_DIGITAL_KEY_EDIT = 'digital_key:edit_by_id'
 URL_LOCATION_ALL = 'digital_key:loc_all'
 URL_LOCATION_DETAIL = 'digital_key:loc_detail'
 URL_SYSTEM_ALL = 'digital_key:sys_all'
 URL_SYSTEM_DETAIL = 'digital_key:sys_detail'
 URL_ASSIGN_ALL = 'digital_key:asgn_all'
 URL_ASSIGN_DETAIL = 'digital_key:asgn_detail'
+
+DATE_FORMAT = '%d.%m.%Y'
 
 
 class DigitalKeyViewTest(TestCase):
@@ -66,6 +73,11 @@ class DigitalKeyViewTest(TestCase):
         self.assertNotContains(response, key.pin_admin)
         self.assertNotContains(response, key.pin_user)
 
+    def test_url_key_edit_redirect(self):
+        key = create_key('Test key')
+        response = self.client.get(reverse(URL_DIGITAL_KEY_EDIT, args=(key.id,)))
+        self.assertEquals(response.status_code, 302)
+
 
 class DigitalKeyViewTestNoLogin(TestCase):
     # w/o logged in
@@ -82,6 +94,11 @@ class DigitalKeyViewTestNoLogin(TestCase):
     def test_url_detail_simple(self):
         key = create_key('Test key')
         response = self.client.get(reverse(URL_DIGITAL_KEY_DETAIL, args=(key.id,)))
+        self.assertEquals(response.status_code, 302)
+
+    def test_url_key_edit_redirect(self):
+        key = create_key('Test key')
+        response = self.client.get(reverse(URL_DIGITAL_KEY_EDIT, args=(key.id,)))
         self.assertEquals(response.status_code, 302)
 
 
@@ -123,6 +140,65 @@ class DigitalKeyViewTestStaff(TestCase):
         self.assertContains(response, key.pin_container)
         self.assertContains(response, key.pin_admin)
         self.assertContains(response, key.pin_user)
+
+    def test_key_edit_simple(self):
+        key = create_key('TEST KEY')
+        response = self.client.get(reverse(URL_DIGITAL_KEY_EDIT, args=(key.id,)))
+        self.assertEquals(response.status_code, 200)
+
+    def test_key_edit_url(self):
+        loc = create_location('TEST-LOC-1')
+        sys = create_work_system('TEST-SYS-1')
+        key = create_key('TEST-KEY-1')
+        key.pin_container = 'PIN-CON-123'
+        key.pin_admin = 'PIN-ADM-123'
+        key.pin_user = 'PIN-USR-123'
+        key.location = loc
+        key.work_systems.add(sys)
+        key.save()
+        key_url = reverse(URL_DIGITAL_KEY_DETAIL, args=(key.id,))
+        response = self.client.get(key_url)
+        self.assertContains(response, key.name)
+        self.assertContains(response, loc.name)
+        self.assertContains(response, sys.name)
+        self.assertContains(response, key.pin_container)
+        self.assertContains(response, key.pin_admin)
+        self.assertContains(response, key.pin_user)
+
+    def test_key_edit(self):
+        key = create_key('KEY-NAME-OLD')
+        form = DigitalKeyForm()
+        _name = 'KEY-NAME-NEW'
+        _serial = 'KEY-SERIAL-NEW'
+        _desc = 'KEY-DESC-NEW'
+        _cert_num = 'KEY-CERT-NUM-NEW'
+        _renew = 10
+        _begin = '01.07.2017'
+        _expire = '01.08.2017'
+        _checked = '21.07.2017'
+        data = {
+            'name': _name,
+            'serial': _serial,
+            'description': _desc,
+            'cert_num': _cert_num,
+            'renewal_time': _renew,
+            'date_begin': _begin,
+            'date_expire': _expire,
+            'date_checked': _checked,
+        }
+        _url = reverse(URL_DIGITAL_KEY_EDIT, args=(key.id,))
+        response = self.client.post(_url, data=data)
+        # self.assertFormError(response, "digitalkey_form", "name", errors=[])
+        self.assertRedirects(response, reverse(URL_DIGITAL_KEY_DETAIL, args=(key.id,)))
+        _key = DigitalKey.objects.get(pk=key.id)
+        self.assertEquals(_key.name, _name)
+        self.assertEquals(_key.serial, _serial)
+        self.assertEquals(_key.description, _desc)
+        self.assertEquals(_key.cert_num, _cert_num)
+        self.assertEquals(_key.renewal_time, _renew)
+        self.assertEquals(_key.date_begin, datetime.strptime(_begin, DATE_FORMAT).date())
+        self.assertEquals(_key.date_expire, datetime.strptime(_expire, DATE_FORMAT).date())
+        self.assertEquals(_key.date_checked, datetime.strptime(_checked, DATE_FORMAT).date())
 
 
 class KeyLocationViewTest(TestCase):
@@ -187,3 +263,33 @@ class AssignmentsViewTest(TestCase):
         assign = create_assignment('TEST-ASSING')
         response = self.client.get(reverse(URL_ASSIGN_DETAIL, args=(assign.id,)))
         self.assertContains(response, assign.name)
+
+
+class FormTest(TestCase):
+    def test_key_edit_form_valid_min(self):
+        key = create_key('KEY-NAME-OLD')
+        form = DigitalKeyForm({
+            'name': 'NEW-NAME-KEY',
+            'serial': 'SERIAL-NEW',
+        }, instance=key)
+        self.assertTrue(form.is_valid(), form.errors)
+        self.assertTrue(form.has_changed())
+        self.assertEquals(key, form.save())
+
+    def test_key_edit_form_valid(self):
+        key = create_key('KEY-NAME-OLD')
+        data = {
+            'name': 'KEY-NAME-NEW',
+            'serial': 'KEY-SERIAL-NEW',
+            'description': 'KEY-DESC-NEW',
+            'cert_num': 'KEY-CERT-NUM-NEW',
+            'renewal_time': 1,
+            'date_begin': '01.07.2017',
+            'date_expire': '01.08.2017',
+            'date_checked': '21.07.2017',
+        }
+        form = DigitalKeyForm(data, instance=key)
+        self.assertTrue(form.is_valid())
+        self.assertTrue(form.has_changed())
+        _key = form.save()
+        self.assertEquals(key, _key)
